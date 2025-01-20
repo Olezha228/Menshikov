4d002f5a3c24514ed5ce9c9d03988f125e260c0b
Oleg Menshikov <olegfide@mail.ru>
2025-01-20 21:04:40 +0500

    fix point.

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -38,7 +38,7 @@ namespace WindowsFormsApp1
             // Добавляем точки данных в серию
             series.Points.AddXY(1, 10);
             series.Points.AddXY(2, 20);
-            series.Points.AddXY(3, 15);
+            series.Points.AddXY(3, 5);
             series.Points.AddXY(4, 25);
             series.Points.AddXY(5, 30);
             series.Points.AddXY(6, 25);
